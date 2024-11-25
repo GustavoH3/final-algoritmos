85b733d6c394ceff0525ca2a8fa6e31267b27c21
gabrielReisPR <gabrielreis0506@gmail.com>
2024-11-24 22:01:14 -0300

    Update Main.java

--- src/finalAlgoritmo/Main.java
+++ src/finalAlgoritmo/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Pessoa joao = new Pessoa();
-        System.out.println("ID do usuário: " + joao.getId());
+        System.out.println("ID do usuárioo: " + joao.getId());
 	}
 }
 
